62197ec008d8e9a41e5916fd8f498972be9b17ba
Henk Kampman <henk.kampman@secondmove.com>
2024-07-29 13:06:22 +0200

    Initial Commit

---
+++ Expression/ExpressionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpressionApp.swift
+//  Expression
+//
+//  Created by Henk on 29/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpressionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
